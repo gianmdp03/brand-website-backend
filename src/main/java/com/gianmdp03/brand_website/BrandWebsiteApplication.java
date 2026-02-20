81f346990150d0e946b8c113acf58327a225662b
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-02-20 11:25:22 -0300

    first commit

---
+++ src/main/java/com/gianmdp03/brand_website/BrandWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.gianmdp03.brand_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gianmdp03/brand_website/BrandWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gianmdp03.brand_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
