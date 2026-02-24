de2764bc2b2710458da911aa9e46f0365e9f4012
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-02-24 16:47:34 -0300

    Add post service

--- src/main/java/com/gianmdp03/brand_website/service/PostService.java
+++ src/main/java/com/gianmdp03/brand_website/service/PostService.java
@@ -1,4 +1,14 @@
 package com.gianmdp03.brand_website.service;
 
+import com.gianmdp03.brand_website.dto.post.PostDetailDTO;
+import com.gianmdp03.brand_website.dto.post.PostRequestDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface PostService {
+    PostDetailDTO addPost(PostRequestDTO dto);
+    PostDetailDTO getPostById(Long id);
+    Page<PostDetailDTO> listPosts(Pageable pageable);
+    PostDetailDTO updatePost(Long id, PostRequestDTO dto);
+    void deletePost(Long id);
 }
--- src/main/java/com/gianmdp03/brand_website/service/impl/PostServiceImpl.java
+++ src/main/java/com/gianmdp03/brand_website/service/impl/PostServiceImpl.java
@@ -1,6 +1,61 @@
 package com.gianmdp03.brand_website.service.impl;
 
+import com.gianmdp03.brand_website.dto.post.PostDetailDTO;
+import com.gianmdp03.brand_website.dto.post.PostRequestDTO;
+import com.gianmdp03.brand_website.exception.NotFoundException;
+import com.gianmdp03.brand_website.mapper.PostMapper;
+import com.gianmdp03.brand_website.model.Post;
+import com.gianmdp03.brand_website.repository.PostRepository;
 import com.gianmdp03.brand_website.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PostServiceImpl implements PostService {
+    private final PostRepository repository;
+    private final PostMapper mapper;
+
+    @Override
+    @Transactional
+    public PostDetailDTO addPost(PostRequestDTO dto) {
+        Post post = mapper.toEntity(dto);
+        return mapper.toDetailDto(repository.save(post));
+    }
+
+    @Override
+    public PostDetailDTO getPostById(Long id) {
+        Post post = repository.findById(id)
+                .orElseThrow(()-> new NotFoundException("Post ID does not exist"));
+        return mapper.toDetailDto(post);
+    }
+
+    @Override
+    public Page<PostDetailDTO> listPosts(Pageable pageable) {
+        Page<Post> page = repository.findAll(pageable);
+        if(page.isEmpty()){
+            return Page.empty();
+        }
+        return page.map(mapper::toDetailDto);
+    }
+
+    @Override
+    @Transactional
+    public PostDetailDTO updatePost(Long id, PostRequestDTO dto) {
+        Post post = repository.findById(id).orElseThrow(()-> new NotFoundException("Post ID does not exist"));
+        mapper.updateEntityFromDto(dto, post);
+        return mapper.toDetailDto(repository.save(post));
+    }
+
+    @Override
+    @Transactional
+    public void deletePost(Long id) {
+        Post post = repository.findById(id)
+                .orElseThrow(()-> new NotFoundException("Post ID does not exist"));
+        repository.delete(post);
+    }
 }
