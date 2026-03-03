acd07c90b49cdfb7ce7ede65ba006870d417ab6a
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-03-03 16:06:18 -0300

    Update BrandWebsiteApplication.java

--- src/main/java/com/gianmdp03/brand_website/BrandWebsiteApplication.java
+++ src/main/java/com/gianmdp03/brand_website/BrandWebsiteApplication.java
@@ -2,11 +2,13 @@ package com.gianmdp03.brand_website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @SpringBootApplication
 public class BrandWebsiteApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(BrandWebsiteApplication.class, args);
 	}
 
