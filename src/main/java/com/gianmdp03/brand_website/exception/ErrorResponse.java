505880938d4f8355a73ae2de4cbe009770b57a1e
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-03-03 16:11:41 -0300

    Add GlobalExceptionHandler

--- src/main/java/com/gianmdp03/brand_website/exception/ErrorResponse.java
+++ src/main/java/com/gianmdp03/brand_website/exception/ErrorResponse.java
@@ -1,4 +1,22 @@
 package com.gianmdp03.brand_website.exception;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class ErrorResponse {
+    private int code;
+    private String error;
+    private String message;
+    private LocalDateTime timestamp;
+
+    public ErrorResponse(int code, String error, String message) {
+        this.code = code;
+        this.error = error;
+        this.message = message;
+        this.timestamp = LocalDateTime.now();
+    }
 }
